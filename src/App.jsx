f1d7171b45e06ef50094e5afa0972a10747510f1
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-10-16 14:08:35 +0100

    fix ui bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Community, Create, Error, Explore, Landing, Vote } from "./routes";
 import { MainUI, Scroll } from "./utils";
-import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { WagmiProvider,createConfig,http } from "wagmi";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { WagmiProvider, createConfig, http } from "wagmi";
 import { base, baseSepolia } from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; 
-import { coinbaseWallet, injected } from 'wagmi/connectors';
-import '@coinbase/onchainkit/styles.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { coinbaseWallet, injected } from "wagmi/connectors";
+import "@coinbase/onchainkit/styles.css";
 
 import Media from "react-media";
 
@@ -61,53 +61,46 @@ function App() {
 			},
 		]);
 
-		const
-		queryClient = new QueryClient(),
-		chains = [base,baseSepolia],
-	  
-		client = createConfig(
-		  {
+	const queryClient = new QueryClient(),
+		chains = [base, baseSepolia],
+		client = createConfig({
 			chains: [baseSepolia],
 			multiInjectedProviderDiscovery: true,
 			connectors: [
-			  
-			  coinbaseWallet({
-				appName: 'citizen',
-				preference: 'smartWalletOnly', // set this to `all` to use EOAs as well
-				version: '4',
-			  }),
-			  injected(),
+				coinbaseWallet({
+					appName: "citizen",
+					preference: "smartWalletOnly", // set this to `all` to use EOAs as well
+					version: "4",
+				}),
+				injected(),
 			],
 			ssr: true,
 			transports: {
-			  [base.id]: http(),
-			  [baseSepolia.id]: http(),
+				[base.id]: http(),
+				[baseSepolia.id]: http(),
 			},
-		  }
-		);	  
+		});
 	return (
 		<>
-			 <WagmiProvider config={client}>
-      <QueryClientProvider client={queryClient}>
-      <OnchainKitProvider chain={baseSepolia}>
-	
-				<Media query={`(min-width:768px)`}>
-					{(matches) => {
-						return matches ? (
-							<section className="container">
-								<RouterProvider router={router} />
-							</section>
-						) : (
-							<section className="query">
-								<Error />
-							</section>
-						);
-					}}
-				</Media>
-
-			</OnchainKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+			<WagmiProvider config={client}>
+				<QueryClientProvider client={queryClient}>
+					<OnchainKitProvider chain={baseSepolia}>
+						<Media query={`(min-width:768px)`}>
+							{(matches) => {
+								return matches ? (
+									<section className="app">
+										<RouterProvider router={router} />
+									</section>
+								) : (
+									<section className="query">
+										<Error />
+									</section>
+								);
+							}}
+						</Media>
+					</OnchainKitProvider>
+				</QueryClientProvider>
+			</WagmiProvider>
 		</>
 	);
 }
